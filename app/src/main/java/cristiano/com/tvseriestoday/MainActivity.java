492e6be54b74e2f95f44f64e0d1b88ae50249e9a
CristianoFunari <raghnar00@gmail.com>
2015-06-04 20:31:26 +0200

    Release

--- app/src/main/java/cristiano/com/tvseriestoday/MainActivity.java
+++ app/src/main/java/cristiano/com/tvseriestoday/MainActivity.java
@@ -1,35 +1,59 @@
 package cristiano.com.tvseriestoday;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+
+import java.text.ParseException;
+
+import cristiano.com.tvseriestoday.sync.TvSeriesSyncAdapter;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements TvSeriesFragment.Callback{
+
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
+    private static final String DETAILFRAGMENT_TAG = "DFTAG";
+
+    private boolean mTwoPane;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
 
+        setContentView(R.layout.activity_main);
+
+        if (findViewById(R.id.tvSeries_detail_container) != null) {
+            // The detail container view will be present only in the large-screen layouts
+            // (res/layout-sw400dp). If this view is present, then the activity should be
+            // in two-pane mode.
+            mTwoPane = true;
+            // In two-pane mode, show the detail view in this activity by
+            // adding or replacing the detail fragment using a
+            // fragment transaction.
+            if (savedInstanceState == null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.tvSeries_detail_container, new DetailFragment(), DETAILFRAGMENT_TAG)
+                        .commit();
+            }
+        } else {
+            mTwoPane = false;
+            getSupportActionBar().setElevation(0f);
+        }
+        TvSeriesFragment tvSeriesFragment =  ((TvSeriesFragment)getSupportFragmentManager()
+                .findFragmentById(R.id.fragment_main));
+
+
+        TvSeriesSyncAdapter.initializeSyncAdapter(this);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -40,27 +64,53 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
+
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
+        if (id == R.id.action_calendar) {
+            try {
+                DetailFragment da = (DetailFragment)getSupportFragmentManager().findFragmentById(R.id.tvSeries_detail_container);
+                da.shareOnCalendar();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            return true;
+        }
+
 
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
+    @Override
+    protected void onResume() {
+        super.onResume();
+            TvSeriesFragment tf = (TvSeriesFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_main);
+                tf.sync();
+    }
 
-        public PlaceholderFragment() {
-        }
+    @Override
+    public void onItemSelected(Uri contentUri) {
+        if (mTwoPane) {
+            // In two-pane mode, show the detail view in this activity by
+            // adding or replacing the detail fragment using a
+            // fragment transaction.
+            Bundle args = new Bundle();
+            args.putParcelable(DetailFragment.DETAIL_URI, contentUri);
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
+            DetailFragment fragment = new DetailFragment();
+            fragment.setArguments(args);
+
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.tvSeries_detail_container, fragment, DETAILFRAGMENT_TAG)
+                    .commit();
+        } else {
+            Intent intent = new Intent(this, DetailActivity.class)
+                    .setData(contentUri);
+            startActivity(intent);
         }
     }
+
+
 }

c0cfcfeb28bc4d2d12abbd4588f035326b0a38ac
CristianoFunari <raghnar00@gmail.com>
2015-06-04 19:54:08 +0200

    Revert "release"
    This reverts commit f2353106f0b3ec1e24850fb3695ed1ec102f878e.

--- app/src/main/java/cristiano/com/tvseriestoday/MainActivity.java
+++ app/src/main/java/cristiano/com/tvseriestoday/MainActivity.java
@@ -1,6 +1,5 @@
 package cristiano.com.tvseriestoday;
 
-<<<<<<< HEAD
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -14,32 +13,10 @@ import android.os.Build;
 
 
 public class MainActivity extends ActionBarActivity {
-=======
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-
-import java.text.ParseException;
-
-import cristiano.com.tvseriestoday.sync.TvSeriesSyncAdapter;
-
-
-public class MainActivity extends ActionBarActivity implements TvSeriesFragment.Callback{
-
-    private final String LOG_TAG = MainActivity.class.getSimpleName();
-    private static final String DETAILFRAGMENT_TAG = "DFTAG";
-
-    private boolean mTwoPane;
-
->>>>>>> origin/Version-1.0
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
@@ -48,43 +25,11 @@ public class MainActivity extends ActionBarActivity implements TvSeriesFragment.
         }
     }
 
-=======
-
-        setContentView(R.layout.activity_main);
-
-        if (findViewById(R.id.tvSeries_detail_container) != null) {
-            // The detail container view will be present only in the large-screen layouts
-            // (res/layout-sw400dp). If this view is present, then the activity should be
-            // in two-pane mode.
-            mTwoPane = true;
-            // In two-pane mode, show the detail view in this activity by
-            // adding or replacing the detail fragment using a
-            // fragment transaction.
-            if (savedInstanceState == null) {
-                getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.tvSeries_detail_container, new DetailFragment(), DETAILFRAGMENT_TAG)
-                        .commit();
-            }
-        } else {
-            mTwoPane = false;
-            getSupportActionBar().setElevation(0f);
-        }
-        TvSeriesFragment tvSeriesFragment =  ((TvSeriesFragment)getSupportFragmentManager()
-                .findFragmentById(R.id.fragment_main));
-
-
-        TvSeriesSyncAdapter.initializeSyncAdapter(this);
-    }
->>>>>>> origin/Version-1.0
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-<<<<<<< HEAD
         getMenuInflater().inflate(R.menu.menu_main, menu);
-=======
-        getMenuInflater().inflate(R.menu.main, menu);
->>>>>>> origin/Version-1.0
         return true;
     }
 
@@ -95,7 +40,6 @@ public class MainActivity extends ActionBarActivity implements TvSeriesFragment.
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-<<<<<<< HEAD
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
@@ -119,55 +63,4 @@ public class MainActivity extends ActionBarActivity implements TvSeriesFragment.
             return rootView;
         }
     }
-=======
-
-        if (id == R.id.action_settings) {
-            startActivity(new Intent(this, SettingsActivity.class));
-            return true;
-        }
-        if (id == R.id.action_calendar) {
-            try {
-                DetailFragment da = (DetailFragment)getSupportFragmentManager().findFragmentById(R.id.tvSeries_detail_container);
-                da.shareOnCalendar();
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-            return true;
-        }
-
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-            TvSeriesFragment tf = (TvSeriesFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_main);
-                tf.sync();
-    }
-
-    @Override
-    public void onItemSelected(Uri contentUri) {
-        if (mTwoPane) {
-            // In two-pane mode, show the detail view in this activity by
-            // adding or replacing the detail fragment using a
-            // fragment transaction.
-            Bundle args = new Bundle();
-            args.putParcelable(DetailFragment.DETAIL_URI, contentUri);
-
-            DetailFragment fragment = new DetailFragment();
-            fragment.setArguments(args);
-
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.tvSeries_detail_container, fragment, DETAILFRAGMENT_TAG)
-                    .commit();
-        } else {
-            Intent intent = new Intent(this, DetailActivity.class)
-                    .setData(contentUri);
-            startActivity(intent);
-        }
-    }
-
-
->>>>>>> origin/Version-1.0
 }
